d6b63843bde25189de557d42537cf0ea396535e6
Singh A. <4rshfn@gmail.com>
2024-04-29 10:14:04 +0200

    prendo il progetto da github facendo il pull

---
+++ src/main/java/com/mycompany/progetto/informatica/ProgettoInformatica.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.progetto.informatica;
+
+/**
+ *
+ * @author Studente
+ */
+public class ProgettoInformatica {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
